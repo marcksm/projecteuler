a5e70647d0074e597750f4e57ad5465973c8e61b
Marcos Vinicius do Carmo Sousa <marcos.vinicius.wz@gmail.com>
2017-12-09 02:28:42 -0200

    problem 5 in python done

--- p5/p.py
+++ p5/p.py
@@ -11,12 +11,33 @@ def nextprime(i):
         i = i + 1
 
 def getprimefactor(n):
-    for x in range (1, n):
-        k = nextprime(x)
+    l=[]
+    x = 2
+    while n > 1:
+        if n%x == 0:
+            l.append(x)
+            n = n/x
+        else:
+            x = nextprime(x)
+    return l
 
+def run():
+    l=[]
+    tmp=[]
+    for x in range (1, 21):
+       tmp=getprimefactor(x)
+       for member in tmp:
+           if (tmp.count(member) > l.count(member)):
+               s = tmp.count(member) - l.count(member)
+               for k in range (1, s+1):
+                   l.append(member)
+    total=1
+    for x in l:
+        total=total*x
+    print (total)
 
+run()
 
-nextprime(2)
 
 
 
